59e828d6cf131d093b9dd637dc7b2c654612845b
1Bronze <khdkkhdd@gmail.com>
2023-12-15 22:44:49 +0900

    관리자 페이지 구현

--- database/sql.js
+++ database/sql.js
@@ -23,9 +23,19 @@ export const selectSql = {
     const [rows] = await promisePool.query(`select * from doctor`);
     return rows;
   },
+  getDoctor: async (doctor_id) => {
+    const query = `SELECT * FROM doctor WHERE doctor_id = ?`;
+    const [rows] = await promisePool.query(query, [doctor_id]);
+    return rows[0];
+  },
   getNurses: async () => {
     const [rows] = await promisePool.query(`select * from nurse`);
     return rows;
+  },
+  getNurse: async (nurse_id) => {
+    const query = `SELECT * FROM nurse WHERE nurse_id = ?`;
+    const [rows] = await promisePool.query(query, [nurse_id]);
+    return rows[0];
   },
   getPatients: async () => {
     const [rows] = await promisePool.query(`select * from patient`);
@@ -34,12 +44,52 @@ export const selectSql = {
 }
 
 export const createSql = {
-//   addClass: async (data) => {
-//     const uid = await promisePool.query(`select Id from Student where StudentId=${data.sId}`);
-//     console.log(uid);
-//     const results = await promisePool.query (
-//       `insert into class_student values (${uid[0][0].Id}, ${data.cId});`
-//     )
-//     return results[0];
-//   }
+  createDoctor: async (doctor_id, name, address, phone_number, password, department_id) => {
+    const query = `
+        INSERT INTO inha_hospital.doctor (doctor_id, name, address, phone_number, password, department_id)
+        VALUES (?, ?, ?, ?, ?, ?)
+    `;
+    const values = [doctor_id, name, address, phone_number, password, department_id];
+    await promisePool.query(query, values);
+  },
+  createNurse: async (nurse_id, name, address, phone_number, password, department_id) => {
+    const query = `
+        INSERT INTO inha_hospital.nurse (nurse_id, name, address, phone_number, password, department_id)
+        VALUES (?, ?, ?, ?, ?, ?)
+    `;
+    const values = [nurse_id, name, address, phone_number, password, department_id];
+    await promisePool.query(query, values);
+  }
+}
+
+export const updateSql = {
+  updateDoctor: async (name, address, phone_number, department_id, doctor_id) => {
+    const query = `
+        UPDATE doctor 
+        SET name = ?, address = ?, phone_number = ?, department_id = ? 
+        WHERE doctor_id = ?
+    `;
+    const values = [name, address, phone_number, department_id, doctor_id];
+    await promisePool.query(query, values);
+  },
+  updateNurse: async (name, address, phone_number, department_id, nurse_id) => {
+    const query = `
+        UPDATE nurse 
+        SET name = ?, address = ?, phone_number = ?, department_id = ? 
+        WHERE nurse_id = ?
+    `;
+    const values = [name, address, phone_number, department_id, nurse_id];
+    await promisePool.query(query, values);
+  }
+}
+
+export const deleteSql = {
+  deleteDoctor: async (doctor_id) => {
+    const query = `DELETE FROM doctor WHERE doctor_id = ?`;
+    await promisePool.query(query, [doctor_id]);
+  },
+  deleteNurse: async (nurse_id) => {
+    const query = `DELETE FROM nurse WHERE nurse_id = ?`;
+    await promisePool.query(query, [nurse_id]);
+  }
 }
--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,7 @@
 import cookieParser from "cookie-parser";
 import express from "express";
 import expressSession from 'express-session';
-import { selectSql } from "../database/sql";
+import { selectSql, createSql, updateSql, deleteSql } from "../database/sql";
 
 const router = express.Router();
 
@@ -12,26 +12,268 @@ router.use(expressSession({
     saveUninitialized: true,
 }))
 
-router.get('/doctor', (req, res) => {
+router.get('/doctor', async (req, res) => {
     const userCookie = JSON.parse(req.cookies.user || '{}');
+    const doctors = await selectSql.getDoctors();
+
     if (userCookie.authority && userCookie.authority === "admin") {
         res.render('admin/doctor-management', {
             username: userCookie.id,
+            doctors: doctors,
         });
     } else {
         res.redirect('/');
     }
 });
 
-router.get('/nurse', (req, res) => {
+router.get('/doctor/create', async (req, res) => {
     const userCookie = JSON.parse(req.cookies.user || '{}');
+    const doctors = await selectSql.getDoctors();
+
+    if (userCookie.authority && userCookie.authority === "admin") {    
+        try {
+            res.render('admin/doctor-management-create', {
+                username: userCookie.id,
+                doctors: doctors,
+            });
+        } catch (e) {
+            res.redirect('/admin/doctor');
+        }
+    } else {
+        res.redirect('/');
+    }
+});
+
+router.post('/doctor/create', async (req, res) => {
+    const userCookie = JSON.parse(req.cookies.user || '{}');
+
+    const doctor_id = req.body.doctor_id;
+    const name = req.body.name;
+    const address = req.body.address;
+    const phone_number = req.body.phone_number;
+    const password = req.body.password;
+    const department_id = req.body.department_id;
+
+    if (userCookie.authority && userCookie.authority === "admin") {
+        try {
+            await createSql.createDoctor(doctor_id, name, address, phone_number, password, department_id);
+
+            res.redirect('/admin/doctor');
+        } catch (e) {
+            res.send(`
+                <script>
+                    alert('Cannot create this! \\nThe department may not exists.');
+                    window.history.back();
+                </script>
+            `);
+        }
+    } else {
+        res.redirect('/');
+    }
+})
+
+router.get('/doctor/update', async (req, res) => {
+    const userCookie = JSON.parse(req.cookies.user || '{}');
+    const doctors = await selectSql.getDoctors();
+    const doctor_id = req.query.id;
+
+    if (userCookie.authority && userCookie.authority === "admin") {    
+        try {
+            const doctorData = await selectSql.getDoctor(doctor_id);
+            res.render('admin/doctor-management-edit', {
+                username: userCookie.id,
+                doctors: doctors,
+                doctorData: doctorData,
+            });
+        } catch (e) {
+            res.redirect('/admin/doctor');
+        }
+    } else {
+        res.redirect('/');
+    }
+});
+
+router.post('/doctor/update', async (req, res) => {
+    const userCookie = JSON.parse(req.cookies.user || '{}');
+
+    const doctor_id = req.body.doctor_id;
+    const name = req.body.name;
+    const address = req.body.address;
+    const phone_number = req.body.phone_number;
+    const department_id = req.body.department_id;
+
+    if (userCookie.authority && userCookie.authority === "admin") {
+        try {
+            await updateSql.updateDoctor(name, address, phone_number, department_id, doctor_id);
+            const doctors = await selectSql.getDoctors();
+
+            res.redirect('/admin/doctor');
+        } catch (e) {
+            res.send(`
+                <script>
+                    alert('Cannot edit this! \\nThe department may not exists.');
+                    window.history.back();
+                </script>
+            `);
+        }
+    } else {
+        res.redirect('/');
+    }
+})
+
+router.get('/doctor/delete', async (req, res) => {
+    const userCookie = JSON.parse(req.cookies.user || '{}');
+    const doctor_id = req.query.id;
+
+    if (userCookie.authority && userCookie.authority === "admin") {
+        if (doctor_id) {
+            try {
+                await deleteSql.deleteDoctor(doctor_id);
+                res.redirect('/admin/doctor');
+            } catch (e) {
+                res.send(`
+                    <script>
+                        alert('Cannot delete this! \\nThe doctor may have patients to care.');
+                        window.history.back();
+                    </script>
+                `);
+            }
+        } else {
+            res.redirect('/');
+        }
+    }
+})
+
+router.get('/nurse', async (req, res) => {
+    const userCookie = JSON.parse(req.cookies.user || '{}');
+    const nurses = await selectSql.getNurses();
+
     if (userCookie.authority && userCookie.authority === "admin") {
         res.render('admin/nurse-management', {
             username: userCookie.id,
+            nurses: nurses,
         });
     } else {
         res.redirect('/');
     }
 });
+
+router.get('/nurse/create', async (req, res) => {
+    const userCookie = JSON.parse(req.cookies.user || '{}');
+    const nurses = await selectSql.getNurses();
+
+    if (userCookie.authority && userCookie.authority === "admin") {    
+        try {
+            res.render('admin/nurse-management-create', {
+                username: userCookie.id,
+                nurses: nurses,
+            });
+        } catch (e) {
+            res.redirect('/admin/nurse');
+        }
+    } else {
+        res.redirect('/');
+    }
+});
+
+router.post('/nurse/create', async (req, res) => {
+    const userCookie = JSON.parse(req.cookies.user || '{}');
+
+    const nurse_id = req.body.nurse_id;
+    const name = req.body.name;
+    const address = req.body.address;
+    const phone_number = req.body.phone_number;
+    const password = req.body.password;
+    const department_id = req.body.department_id;
+
+    if (userCookie.authority && userCookie.authority === "admin") {
+        try {
+            await createSql.createNurse(nurse_id, name, address, phone_number, password, department_id);
+
+            res.redirect('/admin/nurse');
+        } catch (e) {
+            res.send(`
+                <script>
+                    alert('Cannot create this! \\nThe department may not exists.');
+                    window.history.back();
+                </script>
+            `);
+        }
+    } else {
+        res.redirect('/');
+    }
+})
+
+router.get('/nurse/update', async (req, res) => {
+    const userCookie = JSON.parse(req.cookies.user || '{}');
+    const nurses = await selectSql.getNurses();
+    const nurse_id = req.query.id;
+
+    if (userCookie.authority && userCookie.authority === "admin") {    
+        try {
+            const nurseData = await selectSql.getNurse(nurse_id);
+            res.render('admin/nurse-management-edit', {
+                username: userCookie.id,
+                nurses: nurses,
+                nurseData: nurseData,
+            });
+        } catch (e) {
+            res.redirect('/admin/nurse');
+        }
+    } else {
+        res.redirect('/');
+    }
+});
+
+router.post('/nurse/update', async (req, res) => {
+    const userCookie = JSON.parse(req.cookies.user || '{}');
+
+    const nurse_id = req.body.nurse_id;
+    const name = req.body.name;
+    const address = req.body.address;
+    const phone_number = req.body.phone_number;
+    const department_id = req.body.department_id;
+
+    if (userCookie.authority && userCookie.authority === "admin") {
+        try {
+            await updateSql.updateNurse(name, address, phone_number, department_id, nurse_id);
+            const nurses = await selectSql.getNurses();
+
+            res.redirect('/admin/nurse');
+        } catch (e) {
+            res.send(`
+                <script>
+                    alert('Cannot edit this! \\nThe department may not exists.');
+                    window.history.back();
+                </script>
+            `);
+        }
+    } else {
+        res.redirect('/');
+    }
+})
+
+router.get('/nurse/delete', async (req, res) => {
+    const userCookie = JSON.parse(req.cookies.user || '{}');
+    const nurse_id = req.query.id;
+
+    if (userCookie.authority && userCookie.authority === "admin") {
+        if (nurse_id) {
+            try {
+                await deleteSql.deleteNurse(nurse_id);
+                res.redirect('/admin/nurse');
+            } catch (e) {
+                res.send(`
+                    <script>
+                        alert('Cannot delete this! \\nThe nurse may have patients to care.');
+                        window.history.back();
+                    </script>
+                `);
+            }
+        } else {
+            res.redirect('/');
+        }
+    }
+})
 
 module.exports = router;
